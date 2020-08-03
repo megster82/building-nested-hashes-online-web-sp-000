df35e5f91b88b99d14a93dce9657f4d93b671f1f
meaghan jennings <meaghanljennings@gmail.com>
2020-08-03 20:18:37 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,9 +5,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        :name => "Benvolio",
+        {:name => "Benvolio",
         :age => "17",
-        :attitude => "worried"
+        :attitude => "worried"}
+        {:name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -15,9 +18,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        :name => "Steven",
+        {:name => "Steven",
         :age => "30",
-        :attitude => "confused"
+        :attitude => "confused"}
+        {:name => "Nurse",
+        :age => "44",
+        :attitude => "worried"}
       ]
    }
 }
